ef903333fb4035818b3a1df41c6d5cf9f4fd6c56
Kvazikot <kvazikot@gmail.com>
2020-12-05 11:04:14 +0300

    implement new algorithm for maze generation

--- Assets/Scripts/MazeGenParallel.cs
+++ Assets/Scripts/MazeGenParallel.cs
@@ -11,14 +11,15 @@ using UnityEngine;
 
 public partial class MazeGenParallel : MonoBehaviour
 {
-    /*
+    
     public (float, float)[] vertexes_d;
     public (int, int)[] edges_d;
     public bool[] visibility_d;
     public bool[] visited;
     public Dictionary<string, int> edge_map;
-    */
+    public Dictionary<int, int> notCellNumbers;
     public bool canDraw;
+    /*
     static (float, float)[] vertexes_d = new (float, float)[] { (-30f, 30f), (0,30), (30,30), (-30,10),
                                               (0, 10), (30,10), (-30,-8), (0,-8), (30,-8)};
 
@@ -35,7 +36,7 @@ public partial class MazeGenParallel : MonoBehaviour
          {"3-0", 6}, {"1-4", 7}, {"2-5", 8}, {"3-6", 9}, {"4-7",10}, {"5-8",11}
     };
 
-
+    */
     const int NOT_VALID_ID = -1;
 
     public struct CreateMazeJob : IJob
@@ -65,7 +66,7 @@ public partial class MazeGenParallel : MonoBehaviour
                         }
             */
 
-        }
+}
 
 
 
@@ -78,23 +79,34 @@ public partial class MazeGenParallel : MonoBehaviour
         Vector3 v0 = new Vector3(v00.Item1, 5, v00.Item2);
         Vector3 v = new Vector3(v00.Item1, 5, v00.Item2);
         int counter = 0;
-        int x_counter = 1;
+        int x_counter = 0;
 
         canDraw = false;
+        int n_vert = (cellsX + 1) * (cellsY + 1);
+        int n_edges = (cellsX ) * (cellsY + 1) * 2;
         //fill vertices
-        while (counter != (cellsX * cellsY * 2 + 1))
+        int y_counter = 0;
+        while (counter != n_vert )
         {
             vertexes_d[counter] = (v.x, v.z);
             v += new Vector3(grid_step, 0, 0);
-            if (x_counter == (cellsX + 1))
+            if (x_counter == cellsX)
             {
                 v.x = v0.x;
                 v.z -= grid_step;
-                x_counter = 1;
+                x_counter = 0;
+                if(!notCellNumbers.ContainsKey(counter))
+                    notCellNumbers.Add(counter, counter);
+                counter++; y_counter++;
             }
             else
+            {
                 x_counter++;
-            counter++;
+                counter++;
+            }
+            if (y_counter == cellsY)
+                if (!notCellNumbers.ContainsKey(counter))
+                    notCellNumbers.Add(counter, counter);
             //Debug.Log("counter xcounter " + counter + " " + x_counter);
         }
 
@@ -102,7 +114,7 @@ public partial class MazeGenParallel : MonoBehaviour
         int e_counter = 0;
         counter = 0;
         x_counter = 0;
-        while ((e_counter != (cellsX + 1) * 2)  )
+        while (e_counter != n_edges / 2   )
         {
             if (x_counter == cellsX )
             {
@@ -118,66 +130,126 @@ public partial class MazeGenParallel : MonoBehaviour
         }
 
         //vertical edges
-        e_counter = (cellsX + 1) * 2;
+        e_counter = n_edges / 2;
         counter = 0;
         x_counter = 0;
-        while ((e_counter  != (cellsX + 1) * 4))
+        while (e_counter  != n_edges)
         {
              x_counter++;
              edges_d[e_counter] = (counter, counter + cellsX + 1);
              counter++; e_counter++;
         }
+
+        //create edge map
+        n_edges = 0;
+        foreach (var e in edges_d)
+        {
+            edge_map[$"{e.Item1}-{e.Item2}"] = n_edges;
+            n_edges++;
+        }
+
         canDraw = true;
         return;
       
     }
-
+    /*
+    Choose the initial cell, mark it as visited and push it to the stack
+    While the stack is not empty
+        Pop a cell from the stack and make it a current cell
+            If the current cell has any neighbours which have not been visited
+                Push the current cell to the stack
+                Choose one of the unvisited neighbours
+                Remove the wall between the current cell and the chosen cell
+                Mark the chosen cell as visited and push it to the stack
+                */
     public void BuildMaze()
     {
-        List<int> wall_list = new List<int>();
-        HashSet<int> neibours = new HashSet<int>();
-        var rand = new System.Random();
-        int rand_idx = getRandomCell();
+        List<int> cell_list = new List<int>();
+        List<int> neibours = new List<int>();
+
+       
         ResetMe();
         CreateGrid();
-        return;
+        var rand = new System.Random();
+        int rand_idx = getRandomCell();      
         visited[rand_idx] = true;
-        AddCellWalls(rand_idx, ref wall_list);
+        cell_list.Add(rand_idx);
         int iters = 0;
-        while (wall_list.Count != 0)
+        int current_cell = 0;
+
+        while (cell_list.Count != 0)
         {
             int n;
-            n = rand.Next(0, wall_list.Count);
             //if ( n >= wall_list.Count) n--;
-            int edge_id = wall_list[n];
-            int cell_id = Math.Min(edges_d[edge_id].Item1, edges_d[edge_id].Item2);
-            int cell_id2 = getNeibourCell(edge_id);
-
-            if (visited[cell_id] && !visited[cell_id2])
+            current_cell = cell_list[cell_list.Count - 1];
+            cell_list.RemoveAt(cell_list.Count - 1);
+            GetUnvisitedNeibours(current_cell, ref neibours);
+            if (neibours.Count != 0)
             {
-                visibility_d[edge_id] = false;
-                AddEdgeNeibours(edge_id, ref neibours);
-                foreach (var id in neibours)
-                    wall_list.Add(id);
-                neibours.Clear();
-                visited[cell_id2] = true;
-            }
-
-            if (visited[cell_id2] && !visited[cell_id])
-            {
-                visibility_d[edge_id] = false;
-                AddEdgeNeibours(edge_id, ref neibours);
-                foreach (var id in neibours)
-                    wall_list.Add(id);
-                neibours.Clear();
+                cell_list.Add(current_cell);
+                n = rand.Next(0, neibours.Count);
+                int cell_id = neibours[n];
+                RemoveWall(current_cell, cell_id);
                 visited[cell_id] = true;
+                cell_list.Add(cell_id);
             }
-
+            neibours.Clear();
             iters++;
-            wall_list.RemoveAt(n);
             if (iters > 10000) break;
         }
         Debug.Log("iters " + iters);
+    }
+
+    int GetEdgeID(int vertex1, int vertex2)
+    {
+        int edge_id=-1;
+        if (!edge_map.TryGetValue($"{vertex1}-{vertex2}", out edge_id))
+            edge_map.TryGetValue($"{vertex2}-{vertex1}", out edge_id);
+        return edge_id;
+    }
+
+    void RemoveWall(int cell1, int cell2)
+    {
+        int edge_id;
+        int d = cell1 - cell2;
+        if (Math.Abs(d) == (cellsX + 1))
+        {
+            int cell = Math.Max(cell1, cell2);
+            if ((edge_id = GetEdgeID(cell, cell + 1))!=-1)
+               visibility_d[edge_id] = false;
+        }
+        if (d == 1)
+        {
+            if ((edge_id = GetEdgeID(cell1, cell1 + cellsX + 1)) != -1)
+                visibility_d[edge_id] = false;
+        }
+        if (d == -1)
+        {
+            if ((edge_id = GetEdgeID(cell2, cell2 + cellsX + 1)) != -1)
+                visibility_d[edge_id] = false;
+        }
+
+    }
+
+    bool validCellNumber(int n)
+    {
+       return (n >= 0) && (n < vertexes_d.Length) && (!notCellNumbers.ContainsKey(n));
+    }
+        
+    void GetUnvisitedNeibours(int current_cell, ref List<int> neibours)
+    {
+        int n = (current_cell + 1);
+        if(validCellNumber(n) && !visited[n] )
+           neibours.Add(n);
+        n = (current_cell - 1);
+        if (validCellNumber(n) && !visited[n] )
+            neibours.Add(n);
+        n = (current_cell + cellsX + 1);
+        if(validCellNumber(n) && !visited[n] )
+            neibours.Add(n);
+        n = (current_cell - cellsX - 1);
+        if (validCellNumber(n) && !visited[n] )
+            neibours.Add(n);
     }
 
     int getRandomCell()
@@ -301,17 +373,17 @@ public partial class MazeGenParallel : MonoBehaviour
 
     public void RegenerateMaze()
     {
-        /*
-        vertexes_d = new (float, float)[cellsX * cellsY + 1];
-        edges_d = new (int, int)[cellsX * cellsY * 3];
-        visibility_d = new bool[cellsX * cellsY * 3];
-        visited = new bool[cellsX * cellsY + 1];
+        int n_vert = (cellsX + 1) * (cellsY + 1);
+        int n_edges = (cellsX) * (cellsY + 1) * 2;
+        vertexes_d = new (float, float)[n_vert + 1];
+        edges_d = new (int, int)[n_edges];
+        visibility_d = new bool[n_edges];
+        visited = new bool[n_edges];
         // creating vertex and edges arrays based on grid topology
         edge_map = new Dictionary<string, int>();
-        */
-
+        notCellNumbers = new Dictionary<int, int>();
         //NativeArray<Cell1> result = new NativeArray<Cell1>((cellsX+2) * (cellsY+2) + 2, Allocator.TempJob);
-        maze_job = new CreateMazeJob
+         maze_job = new CreateMazeJob
         {
             //vertexes = new NativeArray<(float, float)>(vertexes_d.Length, Allocator.Persistent),
             //edges = new NativeArray<(int, int)>(edges_d.Length, Allocator.Persistent),
@@ -319,8 +391,8 @@ public partial class MazeGenParallel : MonoBehaviour
             cellsX = cellsX,
             cellsY = cellsY
         };
-        JobHandle firstHandle = maze_job.Schedule();
-        firstHandle.Complete();
+        //JobHandle firstHandle = maze_job.Schedule();
+        //firstHandle.Complete();
         //DrawCells1(result);
         BuildMaze();
         //maze_job.visibility.Dispose();
